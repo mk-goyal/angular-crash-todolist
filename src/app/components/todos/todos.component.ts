ae16b400fb86421c2cfac7513cc33942db045bac
mk-goyal <mkgoyal@iitk.ac.in>
2020-10-26 15:02:56 +0530

    this is intial commit.

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo'
+import { CommonModule } from "@angular/common";
+import { TodoService } from '../../services/todo.service';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos =todos;
+    });
+  }
+
+  deleteTodo(todo:Todo){
+    // remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+    
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
+  }
+
+}
